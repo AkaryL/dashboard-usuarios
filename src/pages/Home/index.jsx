f5a03cd8b30844e7f5ea9f0f56f2ff803e6ff330
Gabriel Sanchez <gisgarci99@gmail.com>
2025-08-14 15:09:37 -0600

    Changing grid on home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -35,7 +35,7 @@ export default function Home() {
 
   return (
     <div className="max-w-7xl mx-auto p-4 space-y-6">
-      <div className="grid md:grid-cols-2 gap-4">
+      <div className="grid md:grid-cols-[2fr_1fr] gap-4">
         <DataTable
           columns={[
             { key: "fecha", header: "Fecha" },
